e68d2361b932858f224c57b58d77ed92acb45a48
Mirzohid22 <smm2226fstd@gmail.com>
2023-10-18 22:32:59 +0500

    created Header component's interface

--- src/App.js
+++ src/App.js
@@ -1,24 +1,119 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import {
+  Layout,
+  Menu,
+  Form,
+  Select,
+  Slider,
+  InputNumber,
+  Space,
+  Button,
+  theme,
+} from "antd";
+import { DownloadOutlined, SwapOutlined } from "@ant-design/icons";
+import { COUNTRIES } from "./constants";
+
+const { Header, Footer, Content } = Layout;
 
 function App() {
+  const menuItems = [
+    {
+      key: 1,
+      label: "Home",
+    },
+    {
+      key: 2,
+      label: "About",
+    },
+    {
+      key: 3,
+      label: "Contact",
+    },
+  ];
+
+  const [valueOfError, setValueOfError] = useState(0);
+
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Layout>
+      <Header
+        style={{
+          position: "sticky",
+          top: 0,
+          zIndex: 1,
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+          background: colorBgContainer,
+        }}
+      >
+        <Form
+          layout="inline"
+          style={{
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            padding: "10px",
+            gridGap: "30px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Form.Item label="Region">
+            <Select
+              // style={{ width: 120 }}
+              defaultValue={COUNTRIES[0]}
+              options={COUNTRIES}
+            />
+          </Form.Item>
+          <Form.Item label="Errors">
+            <Space align="center"  style={{ height: "100%", display:'flex', alignItems:'center' }}>
+              <Slider
+                defaultValue={0}
+                min={0}
+                max={10}
+                value={valueOfError}
+                onChange={(value) => {
+                  setValueOfError(value);
+                }}
+                style={{ width: 120 }}
+              />
+              <InputNumber
+                value={valueOfError}
+                onChange={(value) => {
+                  setValueOfError(value);
+                }}
+              />
+            </Space>
+          </Form.Item>
+          <Form.Item label="Seed">
+            <Space.Compact>
+              <InputNumber min={0} max={100000} />
+              <Button
+                type="primary"
+                // style={{ background: "green" }}
+                icon={<SwapOutlined />}
+              >
+                Random
+              </Button>
+            </Space.Compact>
+          </Form.Item>
+          <Form.Item>
+            <Button
+              type="primary"
+              // style={{ background: "green" }}
+              icon={<DownloadOutlined />}
+            >
+              Export
+            </Button>
+          </Form.Item>
+        </Form>
+      </Header>
+      <Content></Content>
+      <Footer></Footer>
+    </Layout>
   );
 }
 
---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+export const COUNTRIES = [
+  {
+    value: "Eng",
+    label: "Eng",
+  },
+  {
+    value: "Rus",
+    label: "Rus",
+  },
+  {
+    value: "Ger",
+    label: "Ger",
+  },
+];
